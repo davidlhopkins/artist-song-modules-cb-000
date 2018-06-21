616e184107c47061f29fd759dbb6716020247288
David Hopkins <dlhopkins2@gmail.com>
2018-06-21 05:20:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,9 @@ require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/paramable'
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
